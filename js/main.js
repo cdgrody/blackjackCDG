eb16d346c601bfda89854db299530490a405466d
cdgrody <cdgrody@gmail.com>
2023-01-03 16:06:16 -0800

    functions reorganized based on what they do and code condensed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let playerTotal, dealerTotal;
 const hiddenCard = null;
 let bank;
 let bet = 0;
+let numberOfDecks = 6;
 
 /*----- app's state (variables) -----*/
 let turn, tableState, winner, blackJackState;
@@ -28,22 +29,16 @@ const betEl = document.querySelector(".bank div:last-child");
 /*----- event listeners -----*/
 buttonEl.addEventListener("click", handleButtonClick);
 startOverEl.addEventListener("click", handleRestart);
-chipsEl.addEventListener("click", placeBet);
+chipsEl.addEventListener("click", handleBet);
 
-/*----- functions -----*/
+/*----- ALL FUNCTIONS -----*/
+/*----- Event Handler Functions -----*/
 function handleButtonClick(evt) {
   const btnType = evt.target.innerHTML;
   if (btnType === "Deal") {
     if (tableState === 2) init();
     tableState = 1;
-    turn = -1;
-    dealCard(turn);
-    turn = 1;
-    dealCard(turn);
-    turn = -1;
-    dealCard(turn);
-    turn = 1;
-    dealCard(turn);
+    initialDeal();
     renderChips();
     renderCards();
     renderScore();
@@ -54,19 +49,16 @@ function handleButtonClick(evt) {
   } else if (btnType === "Hit") {
     firstMove = 0;
     dealCard(turn);
-    clearCardRenderings();
     renderButtons();
     renderCards();
     renderScore();
-    if (calculateScores()[0] > 21) checkGameEnd();
+    if (calculateScores()[0] > 21) determineWinner(); //check if the player busted
   } else if (btnType === "Stand") {
     firstMove = 0;
     renderButtons();
     tableState = 2;
-    while (calculateScores()[1] <= 16) {
-      dealerHit();
-    }
-    checkGameEnd();
+    while (calculateScores()[1] <= 16) dealerHit();
+    determineWinner(); //determine the iwnner of the hand
   } else if(btnType === "Double"){
     if (bank - bet >= 0){ //if there is enough money in the bank, double the bet
       firstMove = 0;
@@ -75,20 +67,13 @@ function handleButtonClick(evt) {
       bet += bet;
       renderBank();
       dealCard(turn);
-      while (calculateScores()[1] <= 16) {
-        dealerHit();
-      }
-      checkGameEnd();
-      // renderButtons();
-      // renderCards();
-      // renderScore();
-      // if (calculateScores()[0] > 21) checkGameEnd();
+      while (calculateScores()[1] <= 16) dealerHit();
+      determineWinner();
     } else {
       renderMessageBox();
-      console.log(tableState)
     }
   } else if(btnType === "Play Again"){
-    init();
+    init(); //start a new hand but don't reshuffle or reset the bank
   } else if(btnType === "Reset Bet"){
     bank += bet;
     bet = 0;
@@ -98,7 +83,25 @@ function handleButtonClick(evt) {
     renderMessageBox("Place your bet to begin the game");
   }
 }
+function handleRestart() {
+  tableState = 0;
+  init();
+}
 
+function handleBet(evt) {
+  let betAmount = parseInt(evt.target.innerHTML.split("$")[1]);
+  if (bank - betAmount >= 0) {
+    bet += betAmount;
+    bank -= betAmount;
+    tableState = 3; //ready to show the deal button
+    renderButtons();
+    renderBank();
+  } else {
+    renderMessageBox("Bet amount cannot exceed bank total!");
+  }
+}
+
+/*----- Initialization Functions -----*/
 function init() {
   clearCardRenderings();
   clearHands();
@@ -117,42 +120,6 @@ function init() {
   renderButtons();
 }
 
-function placeBet(evt) {
-  let betAmount = parseInt(evt.target.innerHTML.split("$")[1]);
-  if (bank - betAmount >= 0) {
-    bet += betAmount;
-    bank -= betAmount;
-    tableState = 3; //ready to show the deal button
-    renderButtons();
-    renderBank();
-  } else {
-    renderMessageBox("Bet amount cannot exceed bank total!");
-  }
-}
-
-function renderBank() {
-  bankTotalEl.innerHTML = `Bank: $${bank}`;
-  betEl.innerHTML = `Bet: $${bet}`;
-}
-
-function handleRestart() {
-  tableState = 0;
-  init();
-}
-
-function shuffleDeck() {
-  faces.forEach(function (cardVal) {
-    suits.forEach(function (suit) {
-      cards.push({
-        cardName: cardVal,
-        suit: suit,
-        value: assignCardValue(cardVal),
-        cardString: `card ${suit}${cardVal}`,
-      });
-    });
-  });
-}
-
 function assignCardValue(cardVal) {
   if (parseInt(cardVal)) {
     //if the card is a number, return that number
@@ -166,9 +133,95 @@ function assignCardValue(cardVal) {
   }
 }
 
+function shuffleDeck() {
+  faces.forEach(function (cardVal) {
+    suits.forEach(function (suit) {
+      for(let i = 0; i < numberOfDecks; i++){
+        cards.push({
+          cardName: cardVal,
+          suit: suit,
+          value: assignCardValue(cardVal),
+          cardString: `card ${suit}${cardVal}`,
+        });
+      }
+    });
+  });
+}
+
+/*----- Controller Functions -----*/
+
+function calculateScores() {
+  let playerTotal = 0;
+  let dealerTotal = 0;
+  playerCards.forEach((playerCard) => (playerTotal += playerCard.value));
+  if (playerTotal > 21)
+    playerTotal = aceValueAdjustment(playerCards, playerTotal);
+  dealerCards.forEach((dealerCard) => (dealerTotal += dealerCard.value));
+  if (dealerTotal > 21)
+    dealerTotal = aceValueAdjustment(dealerCards, dealerTotal);
+  return [playerTotal, dealerTotal, dealerTotal - dealerCards[0].value];
+}
+
+function checkBlackJack(){
+  if(calculateScores()[1] === 21 || calculateScores()[0] === 21){
+    tableState = 2;
+    blackJackState = 1;
+    determineWinner();
+  }
+}
+
+function determineWinner() {
+  let scores = calculateScores(); //player is index 0, dealer is index 1
+  if (scores[1] > 21) {
+    renderMessageBox(`Player wins $${bet}, dealer busted`);
+    winner = 1;
+  } else if (scores[0] > 21) {
+    tableState = 2;
+    renderMessageBox(`Dealer wins, player busted and loses $${bet}`);
+    winner = -1;
+  } else if (scores[0] > scores[1]) {
+    if(blackJackState === 1) renderMessageBox(`Blackjack! Player wins $${bet+=1.5*bet}`);
+    renderMessageBox(`Player wins $${bet}`);
+    winner = 1;
+  } else if (scores[0] < scores[1]) {
+    if(blackJackState === 1) renderMessageBox(`Blackjack! Player loses $${bet}`);
+    renderMessageBox(`Dealer wins, player loses $${bet}`);
+    winner = -1;
+  } else if (scores[0] === scores[1]) {
+    renderMessageBox("Draw");
+    winner = 0;
+    bank += bet;
+  }
+  dealerEl.children[0].setAttribute("class", dealerCards[0].cardString); //reveal hidden card!
+  if (winner === 1 && !blackJackState) bank += 2 * bet;
+  if (winner === 1 && !!blackJackState) bank += 2.5 * bet;
+  blackJackState = 0;
+  bet = 0;
+  renderBank();
+  renderButtons();
+  renderScore();
+  if (bank === 0) renderEndgame();
+}
+
+function aceValueAdjustment(deckCards, total) {
+  const aceCount = deckCards.filter((deckCard) => deckCard.cardName === "A");
+  if (aceCount === 0) return total;
+  for (let i = 0; i < aceCount.length; i++) {
+    total -= 10;
+    if (total < 22) return total;
+  }
+  return total;
+}
+
+function dealerHit() {
+  turn = -1;
+  dealCard(turn);
+  renderCards();
+  renderScore();
+}
+
 function dealCard(turn) {
   if (cards.length === 0) {
-    console.log("we're out, lets reshuffle");
     shuffleDeck();
   }
   const cardIdx = Math.floor(Math.random() * cards.length);
@@ -178,6 +231,24 @@ function dealCard(turn) {
     dealerCards.push(cards[cardIdx]);
   }
   cards.splice(cardIdx, 1);
+}
+
+function initialDeal(){
+  turn = -1;
+  dealCard(turn);
+  turn = 1;
+  dealCard(turn);
+  turn = -1;
+  dealCard(turn);
+  turn = 1;
+  dealCard(turn);
+}
+
+
+/*----- Render Functions -----*/
+function renderBank() {
+  bankTotalEl.innerHTML = `Bank: $${bank}`;
+  betEl.innerHTML = `Bet: $${bet}`;
 }
 
 function renderCards() {
@@ -208,60 +279,29 @@ function renderCardOutlines(element) {
 function renderMessageBox(message) {
   messageEl.removeChild(messageEl.children[0]);
   const messageChild = document.createElement("div");
-  if (tableState === 0 || tableState === 3) {
-    // messageChild.innerHTML = "Place your bet to begin the game";
+  if (tableState === 0 || tableState === 3 || tableState === 4) {
     messageChild.innerHTML = `${message}`;
   } else if (tableState === 1) {
-    messageChild.innerHTML = "Hit or Stand?  Make your move...";
+    messageChild.innerHTML = "Make your move...";
     if(firstMove) messageChild.innerHTML = "Sorry. Not enough funds available to double.";
   } else if (tableState === 2) {
     messageChild.innerHTML = `Game over! ${message}!`;
-  } else if(tableState === 4){
-    messageChild.innerHTML = `${message}`;
   }
   messageEl.appendChild(messageChild);
-}
-
-function clearCardRenderings() {
-  const playerDeckLength = playerEl.children.length;
-  const dealerDeckLength = dealerEl.children.length;
-  for (let i = 0; i < playerDeckLength; i++) {
-    playerEl.removeChild(playerEl.children[0]);
-  }
-  for (let i = 0; i < dealerDeckLength; i++) {
-    dealerEl.removeChild(dealerEl.children[0]);
-  }
-}
-
-function clearChipRenderings() {
-  const chipLength = chipsEl.children.length;
-  for (let i = 0; i < chipLength; i++) {
-    chipsEl.removeChild(chipsEl.children[0]);
-  }
 }
 
 function renderChips(){
   clearChipRenderings();
   if (tableState === 0){
-    const bet5 = document.createElement("button");
-    const bet10 = document.createElement("button");
-    const bet20 = document.createElement("button");
-    bet5.innerHTML = "$5"
-    bet10.innerHTML = "$10"
-    bet20.innerHTML = "$20"
-    bet5.setAttribute("class", "preHand");
-    bet5.setAttribute("class", "preHand");
-    bet5.setAttribute("class", "preHand");
-    chipsEl.appendChild(bet5);
-    chipsEl.appendChild(bet10);
-    chipsEl.appendChild(bet20);
-  }
-}
-
-function clearButtonRenderings() {
-  const buttonLength = buttonEl.children.length;
-  for (let i = 0; i < buttonLength; i++) {
-    buttonEl.removeChild(buttonEl.children[0]);
+    bets = ["bet5","bet10","bet20"];
+    betValues = [5, 10, 20];
+    bets.forEach(function(bet){
+      const betValue = bet.split("t")[1];
+      bet = document.createElement("button");
+      bet.innerHTML = `$${betValue}`
+      bet.setAttribute("class", "preHand");
+      chipsEl.appendChild(bet);
+    })
   }
 }
 
@@ -291,23 +331,6 @@ function renderButtons() {
   }
 }
 
-function clearHands() {
-  playerCards.splice(0, playerCards.length);
-  dealerCards.splice(0, dealerCards.length);
-}
-
-function calculateScores() {
-  let playerTotal = 0;
-  let dealerTotal = 0;
-  playerCards.forEach((playerCard) => (playerTotal += playerCard.value));
-  if (playerTotal > 21)
-    playerTotal = aceValueAdjustment(playerCards, playerTotal);
-  dealerCards.forEach((dealerCard) => (dealerTotal += dealerCard.value));
-  if (dealerTotal > 21)
-    dealerTotal = aceValueAdjustment(dealerCards, dealerTotal);
-  return [playerTotal, dealerTotal, dealerTotal - dealerCards[0].value];
-}
-
 function renderScore() {
   if (tableState === 0) {
     resultsEl.children[0].innerHTML = `Dealer: `;
@@ -323,50 +346,6 @@ function renderScore() {
   }
 }
 
-function checkBlackJack(){
-  if(calculateScores()[1] === 21 || calculateScores()[0] === 21){
-    tableState = 2;
-    blackJackState = 1;
-    checkGameEnd();
-  }
-}
-
-function checkGameEnd() {
-  let scores = calculateScores();
-  if (scores[1] > 21) {
-    renderMessageBox(`Player wins $${bet}, dealer busted`);
-    winner = 1;
-  } else if (scores[0] > 21) {
-    tableState = 2;
-    renderMessageBox(`Dealer wins, player busted and loses $${bet}`);
-    winner = -1;
-  } else if (scores[0] > scores[1]) {
-    if(!!blackJackState === 1) renderMessageBox(`Blackjack! Player wins $${bet+=1.5*bet}`);
-    renderMessageBox(`Player wins $${bet}`);
-    winner = 1;
-  } else if (scores[0] < scores[1]) {
-    if(blackJackState === 1) renderMessageBox(`Blackjack! Player loses $${bet}`);
-    renderMessageBox(`Dealer wins, player loses $${bet}`);
-    winner = -1;
-  } else if (scores[0] === scores[1]) {
-    renderMessageBox("Draw");
-    winner = 0;
-    bank += bet;
-  } else {
-    console.log(tableState, "No Result Yet");
-    return;
-  }
-  dealerEl.children[0].setAttribute("class", dealerCards[0].cardString); //reveal hidden card!
-  if (winner === 1 && !blackJackState) bank += 2 * bet;
-  if (winner === 1 && !!blackJackState) bank += 2.5 * bet;
-  blackJackState = 0;
-  bet = 0;
-  renderBank();
-  renderButtons();
-  renderScore();
-  if (bank === 0) renderEndgame();
-}
-
 function renderEndgame(){
   clearButtonRenderings();
   clearChipRenderings();
@@ -374,22 +353,34 @@ function renderEndgame(){
   renderMessageBox("Game over! You ran out of money.")
 }
 
-function aceValueAdjustment(deckCards, total) {
-  const aceCount = deckCards.filter((deckCard) => deckCard.cardName === "A");
-  if (aceCount === 0) return total;
-  for (let i = 0; i < aceCount.length; i++) {
-    total -= 10;
-    if (total < 22) return total;
+function clearHands() {
+  playerCards.splice(0, playerCards.length);
+  dealerCards.splice(0, dealerCards.length);
+}
+
+function clearCardRenderings() {
+  const playerDeckLength = playerEl.children.length;
+  const dealerDeckLength = dealerEl.children.length;
+  for (let i = 0; i < playerDeckLength; i++) {
+    playerEl.removeChild(playerEl.children[0]);
   }
-  return total;
+  for (let i = 0; i < dealerDeckLength; i++) {
+    dealerEl.removeChild(dealerEl.children[0]);
+  }
 }
 
-function dealerHit() {
-  turn = -1;
-  dealCard(turn);
-  renderCards();
-  renderScore();
+function clearChipRenderings() {
+  const chipLength = chipsEl.children.length;
+  for (let i = 0; i < chipLength; i++) {
+    chipsEl.removeChild(chipsEl.children[0]);
+  }
 }
 
+function clearButtonRenderings() {
+  const buttonLength = buttonEl.children.length;
+  for (let i = 0; i < buttonLength; i++) {
+    buttonEl.removeChild(buttonEl.children[0]);
+  }
+}
 //run code
 init();
